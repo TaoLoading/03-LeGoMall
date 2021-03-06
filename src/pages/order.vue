51fdf7975078d3fce51e401568f77d5ae9a22780
TaoLoading <1377226380@qq.com>
2021-03-06 21:20:57 +0800

    修改header重复现象

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,19 +1,24 @@
 <!-- 订单页面(整体框架) -->
 <template>
   <div>
-    <order-header></order-header>
     <router-view></router-view>
+    <service-bar></service-bar>
     <nav-footer></nav-footer>
   </div>
 </template>
-
 <script>
-import OrderHeader from './../components/OrderHeader'
+import ServiceBar from './../components/ServiceBar'
 import NavFooter from './../components/NavFooter'
 export default {
   name: 'order',
+  data() {
+    return {
+      title: '',
+      tip: '',
+    }
+  },
   components: {
-    OrderHeader,
+    ServiceBar,
     NavFooter,
   },
 }
